fbe44d84b287ce6110035c569cc7a9c10eca7d68
jay <maestro1286@gmail.com>
2023-08-05 13:56:51 -0700

    refactors

--- pkg/files/test_utils.go
+++ pkg/files/test_utils.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -9,17 +10,19 @@ import (
 
 // run an individual test as part of a series of larger tests
 func RunTestStage(t *testing.T, stageName string, test func(t *testing.T)) {
-	log.Printf("================================================")
-	log.Printf("Running test stage %s\n", stageName)
+	log.Print(fmt.Sprintf("=============== %s Testing Stage ===============", stageName))
 	test(t)
-	log.Printf("================================================")
+	log.Print("================================================")
 }
 
-// build path to test file directory
+// build path to test file directory. creates testing directory if it doesn't exist.
 func GetTestingDir() string {
 	curDir, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("[ERROR] unable to get testing directory: %v", err)
+		log.Printf("[ERROR] unable to get testing directory: %v\ncreating...", err)
+		if err := os.Mkdir(filepath.Join(curDir, "test_files"), 0666); err != nil {
+			log.Fatalf("[ERROR] unable to create test directory: %v", err)
+		}
 	}
 	return filepath.Join(curDir, "test_files")
 }
