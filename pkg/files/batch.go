6d719be4d8f4685d64f14a1bd6e47bcb943d211e
jay <maestro1286@gmail.com>
2023-08-11 16:44:37 -0700

    refactor

--- pkg/files/batch.go
+++ pkg/files/batch.go
@@ -26,33 +26,13 @@ func NewBatch() *Batch {
 	}
 }
 
-// return the difference between two []*File slices.
-//
-// assuming that go's map implementation has ~O(1) access time,
-// then this function should work in ~O(n) on an unsorted slice.
-//
-// https://stackoverflow.com/questions/19374219/how-to-find-the-difference-between-two-slices-of-strings
-func diff(f, g []*File) []*File {
-	tmp := make(map[*File]string, len(g))
-	for _, file := range g {
-		tmp[file] = "hi"
-	}
-	var diff []*File
-	for _, file := range f {
-		if _, found := tmp[file]; !found {
-			diff = append(diff, file)
-		}
-	}
-	return diff
-}
-
 /*
 TODO: look at the knapsack problem for guidance here.
 
 we're bound  by an upper size limit on our batch sizes (MAX) since
 we ideally don't want to clog a home network's resources when uploading
 or downloading batches of files. MAX is subject to change of course,
-but it's in place as a mechanism for resource management.
+but its in place as a mechanism for resource management.
 */
 
 // NOTE this does not check whether there are any duplicate files in the
@@ -65,12 +45,29 @@ func (b *Batch) AddFiles(files []*File) []*File {
 	notAdded := make([]*File, 0)
 
 	for _, f := range files {
-		// this is basically a greedy approach, but that may
-		// be subect to change.
+		// this is basically a greedy approach, but that may be subect to change.
+		//
+		// the main criteria is essentially that whatever
+		// the size of the *current* file is, it shouldn't cause us to exceed our
+		// capacity (b.Cap = MAX), so even if a list of files has an unsorted
+		// series of valies, we will only ever care about the effect of the *current* file's size
+		// in relation to the remaining capacity of the batch, not the sum total
+		// of the given file list (sorted or otherwise).
+		//
+		// since lists are unsorted, a file that is much larger than its neighbors may cause
+		// batches to not contain as many possible files since one files weight may greatly tip the scales,
+		// as it were. NP problems are hard.
+		//
+		// individual files that exceed b.Cap won't be added to the batch ever.
+		// TODO: investigate this case
 		if b.Cap-f.Size() >= 0 {
 			b.Files = append(b.Files, f)
-			b.Cap -= f.Size()        // update remaning file capacity
+			b.Cap -= f.Size()        // decrement remaning file capacity
 			added = append(added, f) // save to added files list
+			// don't bother checking the rest
+			if b.Cap == 0 {
+				break
+			}
 		} else {
 			// we want to check the other files in this list
 			// since they may be small enough to add onto this batch.
@@ -84,13 +81,15 @@ func (b *Batch) AddFiles(files []*File) []*File {
 		// if all files were successfully added
 		log.Printf("[DEBUG] all files added to batch. remaining batch capacity (in bytes): %d", b.Cap)
 		return added
-	} else if b.Cap == MAX && len(added) < len(files) {
-		// if we reach capacity before we finish with files,
-		// return a list of the remaining files
-		return diff(added, files)
-	} else if len(notAdded) > 0 && b.Cap < MAX {
-		// if b.Cap < MAX and we have left over files that were passed over for
-		// being to large for the current batch
+	}
+	// if we reach capacity before we finish with files,
+	// return a list of the remaining files
+	if b.Cap == MAX && len(added) < len(files) {
+		return Diff(added, files)
+	}
+	// if b.Cap < MAX and we have left over files that were passed over for
+	// being to large for the current batch
+	if len(notAdded) > 0 && b.Cap < MAX {
 		return notAdded
 	}
 	return nil
