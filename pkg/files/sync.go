7a99a94fc4701921beb9dfe2cfbf7526b6ee14fc
jay <maestro1286@gmail.com>
2023-08-11 18:12:40 -0700

    refactors

--- pkg/files/sync.go
+++ pkg/files/sync.go
@@ -9,7 +9,7 @@ type SyncIndex struct {
 
 	// We will use the file path for each file to retrieve the pointer for the
 	// file object if it is to be queued for uploading or downloading
-	// key = file path, value = last modified date
+	// key = file , value = last modified date
 	LastSync map[string]time.Time
 
 	// map of files to be queued for uploading or downloading
@@ -19,8 +19,8 @@ type SyncIndex struct {
 
 func NewSyncIndex() *SyncIndex {
 	return &SyncIndex{
-		LastSync: make(map[string]time.Time),
-		ToUpdate: make(map[string]*File),
+		LastSync: make(map[string]time.Time, 0),
+		ToUpdate: make(map[string]*File, 0),
 	}
 }
 
@@ -50,11 +50,10 @@ but we assume this is the root, and that there is no parent directory!
 utilizes the directory's d.WalkS() function
 */
 func BuildSyncIndex(dir *Directory) *SyncIndex {
-	idx := dir.WalkS()
-	if idx == nil {
-		log.Fatalf("[ERROR] could not build sync index for dir: %v", dir.ID)
+	if idx := dir.WalkS(); idx != nil {
+		return idx
 	}
-	return idx
+	return nil
 }
 
 /*
@@ -71,7 +70,7 @@ func BuildToUpdate(dir *Directory, idx *SyncIndex) *SyncIndex {
 	for _, f := range dir.Files {
 		// check if the time difference between most recent sync and last sync
 		// is greater than zero.
-		if f.LastSync.Sub(idx.LastSync[f.Path]) > 0 {
+		if f.LastSync.Sub(idx.LastSync[f.ID]) > 0 {
 			idx.ToUpdate[f.ID] = f
 		}
 	}
