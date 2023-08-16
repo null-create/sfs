859bc886ebdc59e4a0d0e8c982a511499a381535
jay <maestro1286@gmail.com>
2023-08-15 21:12:18 -0700

    add util

---
+++ pkg/server/utils.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+// write out as a json file
+func save(dir, filename string, data map[string]interface{}) {
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatalf("[ERROR] failed marshalling JSON data: %s\n", err)
+	}
+
+	if err = ioutil.WriteFile(filepath.Join(dir, filename), jsonData, 0666); err != nil {
+		log.Fatalf("[ERROR] unable to write JSON file %s: %s\n", filename, err)
+	}
+}
