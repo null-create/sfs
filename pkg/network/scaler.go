38df89c11cdc3710bd60bfa8f98517722eaada62
jay <maestro1286@gmail.com>
2023-08-16 22:23:41 -0700

    add ScaleBatches()

--- pkg/network/scaler.go
+++ pkg/network/scaler.go
@@ -1,11 +1,12 @@
 package network
 
 /*
-Scalar takes a network profile file, then chooses a range of possible file batch sizes used for uploading or downloading.
-
-Upload/Download pairs will be mapped against a set of file batch sizes.
-
-Batch sizes should be limited to file SIZE rather than file quantity, so as not to accidentally create
-a huge batch of huge files (and clog the user's network).
-
+ScaleBatches() generates a network profile file,
+then chooses a a batch size limit used for uploading or downloading.
 */
+func ScaleBatches() {
+	p := ProfileNetwork()
+
+	// establish network upload/download batch sizes
+	PickMAX(p)
+}
