219315e09ca9afbc28849a870054de2a0fed7459
jay <maestro1286@gmail.com>
2023-08-16 22:24:51 -0700

    add WIP service daemon

---
+++ pkg/daemon.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// start sfs listeer daemon
+func daemon() {
+	log.Printf("[DEBUG] starting sfs daemon...")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// start http listener server. should be able to run in
+	// the background without user interaction.
+
+	// go startHTTPServer()
+
+	<-stop // Block until an interrupt signal is received
+	log.Printf("[DEBUG] received interrupt signal. stopping daemon...")
+}
