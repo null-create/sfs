852bf94321894c143cb175bd18d9216acfd376e7
jay <maestro1286@gmail.com>
2023-08-16 22:24:38 -0700

    move SaveProfile() + refactors

--- pkg/network/utils.go
+++ pkg/network/utils.go
@@ -1,12 +1,22 @@
 package network
 
 import (
-	"encoding/json"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-const PROFILE string = "network-profile.json"
+func ProfileDirPath() string {
+	return filepath.Join(GetCwd(), "profile")
+}
+
+func GetCwd() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("[ERROR] unable to get current working directory %v", err)
+	}
+	return dir
+}
 
 func GetHostName() string {
 	hostname, err := os.Hostname()
@@ -15,30 +25,4 @@ func GetHostName() string {
 		return ""
 	}
 	return hostname
-}
-
-// SaveProfile creates a simple .json file of our network speed averages
-func SaveProfile(profile *NetworkProfile) error {
-
-	// TODO: create dedicated service file path for network profile
-
-	// Open the JSON file for writing
-	file, err := os.Create(PROFILE)
-	if err != nil {
-		log.Fatalf("[ERROR] error creating file \n%v\n ", err)
-	}
-	defer file.Close()
-
-	// Encode the data and write it to the file
-	encoder := json.NewEncoder(file)
-	jsonStr, err := json.Marshal(&profile)
-	if err != nil {
-		log.Fatalf("[ERROR] error marshalling data to JSON format \n%v\n ", err)
-	}
-	err = encoder.Encode(jsonStr)
-	if err != nil {
-		log.Fatalf("[ERROR] error encoding data to JSON \n%v\n ", err)
-	}
-
-	return nil
 }
