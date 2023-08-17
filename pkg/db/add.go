86f853fd621c5e6be2cf0fa987faff8af48d3404
jay <maestro1286@gmail.com>
2023-08-16 19:58:25 -0700

    refactors

--- pkg/db/add.go
+++ pkg/db/add.go
@@ -7,18 +7,47 @@ import (
 	"github.com/sfs/pkg/files"
 )
 
-// Insert query with ON CONFLICT IGNORE (SQLite equivalent of upsert)
-const addQuery = `
-INSERT OR IGNORE INTO Files (id, name, owner, protected, key, path, server_path, client_path, checksum, algorithm)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-`
+// NOTE: these are string literals.
+// may need to have all \t and \n characters
+// removed before processing?
+const (
+	// Insert query with ON CONFLICT IGNORE (SQLite equivalent of upsert)
+	AddFileQuery string = `
+	INSERT OR IGNORE INTO Files (
+		id, 
+		name, 
+		owner, 
+		protected, 
+		key, 
+		path, 
+		server_path, 
+		client_path, 
+		checksum, 
+		algorithm
+	)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+
+	InsertUserQuery string = `
+	INSERT OR IGNORE INTO Users (
+		id, 
+		name, 
+		username, 
+		email, 
+		password, 
+		last_login, 
+		is_admin, 
+		total_files, 
+		total_directories
+	)
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`
+)
 
 func (q *Query) AddFile(f *files.File) error {
 	q.Connect()
 	defer q.Close()
 
 	// prepare query
-	if err := q.Prepare(addQuery); err != nil {
+	if err := q.Prepare(AddFileQuery); err != nil {
 		return fmt.Errorf("[ERROR] failed to prepare statement: %v", err)
 	}
 	defer q.Stmt.Close()
@@ -41,18 +70,18 @@ func (q *Query) AddFile(f *files.File) error {
 	return nil
 }
 
-func (q *Query) AddFiles(f *[]files.File) error {
+func (q *Query) AddFiles(fs *[]files.File) error {
 	q.Connect()
 	defer q.Close()
 
 	// prepare query
-	if err := q.Prepare(addQuery); err != nil {
+	if err := q.Prepare(AddFileQuery); err != nil {
 		return fmt.Errorf("[ERROR] failed to prepare statement: %v", err)
 	}
 	defer q.Stmt.Close()
 
 	// iterate over the files and execute the statement for each
-	for _, file := range *f {
+	for _, file := range *fs {
 		if _, err := q.Stmt.Exec(
 			&file.ID,
 			&file.Name,
