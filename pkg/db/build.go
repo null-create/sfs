47cdaf09d68dfdb4edc2552d224d86e85f3af482
jay <maestro1286@gmail.com>
2023-08-18 14:13:05 -0700

    add tmp db method

--- pkg/db/build.go
+++ pkg/db/build.go
@@ -22,6 +22,20 @@ func NewDB(dbName string, pathToNewDB string) {
 	}
 }
 
+// build a temp testing directory using a specified SQL query
+func NewTestDB(dbName string, pathToNewDB string, query string) {
+	db, err := sql.Open("sqlite3", pathToNewDB)
+	if err != nil {
+		log.Fatalf("[ERROR] unable to open database: \n%v\n", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatalf("[ERROR] failed to create table: \n%v\n", err)
+	}
+}
+
 func new(path string, query string) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
