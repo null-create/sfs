fa636e74ce69bb87e65ffaac864222b601e7f810
jay <maestro1286@gmail.com>
2023-08-18 14:16:47 -0700

    update test

--- pkg/db/build_test.go
+++ pkg/db/build_test.go
@@ -12,10 +12,10 @@ func TestBuildDbs(t *testing.T) {
 	testDir := GetTestingDir(t)
 
 	// ------- create tmp databases
-	NewDB("users", filepath.Join(testDir, "users"))
-	NewDB("files", filepath.Join(testDir, "files"))
-	NewDB("drives", filepath.Join(testDir, "drives"))
-	NewDB("directories", filepath.Join(testDir, "directories"))
+	New("users", filepath.Join(testDir, "users"))
+	New("files", filepath.Join(testDir, "files"))
+	New("drives", filepath.Join(testDir, "drives"))
+	New("directories", filepath.Join(testDir, "directories"))
 
 	// make sure we created the database files
 	entries, err := os.ReadDir(testDir)
