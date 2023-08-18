8e456aced3ab081c1c112dc0b59aab8d868efaa6
jay <maestro1286@gmail.com>
2023-08-18 14:14:30 -0700

    move to separate modules

--- pkg/db/add_test.go
+++ pkg/db/add_test.go
@@ -1,1 +1,35 @@
 package db
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestAddUser(t *testing.T) {
+	testDir := GetTestingDir(t)
+
+	// make testing objects
+	_, _, tmpUser := MakeTestItems(t, testDir)
+
+	// ------- try to make simple queries to each
+	q1 := NewQuery(filepath.Join(testDir, "users"))
+
+	// add test user
+	q1.Connect()
+	if err := q1.AddUser(tmpUser); err != nil {
+		t.Fatalf("[ERROR] failed to add user: %v", err)
+	}
+	q1.Close()
+
+	// add test drive
+
+	// add test directory
+
+	// query for each item in each db to make sure they were added successfully
+}
+
+func TestAddFile(t *testing.T) {}
+
+func TestAddDirectory(t *testing.T) {}
+
+func TestAddDrive(t *testing.T) {}
--- pkg/db/build_test.go
+++ pkg/db/build_test.go
@@ -1,1 +1,29 @@
 package db
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestBuildDbs(t *testing.T) {
+	testDir := GetTestingDir(t)
+
+	// ------- create tmp databases
+	NewDB("users", filepath.Join(testDir, "users"))
+	NewDB("files", filepath.Join(testDir, "files"))
+	NewDB("drives", filepath.Join(testDir, "drives"))
+	NewDB("directories", filepath.Join(testDir, "directories"))
+
+	// make sure we created the database files
+	entries, err := os.ReadDir(testDir)
+	if err != nil {
+		t.Fatalf("[ERROR] failed to read testing directory: %v", err)
+	}
+	assert.NotEqual(t, 0, len(entries))
+
+	// ------- clean up temporary databases
+	Clean(t, testDir)
+}
