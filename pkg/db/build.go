fddd11ee6c38e152ec66882b27564e8c7477ccdc
jay <maestro1286@gmail.com>
2023-08-18 14:16:15 -0700

    make New exportable

--- pkg/db/build.go
+++ pkg/db/build.go
@@ -10,33 +10,20 @@ import (
 // all database files must end with .db!
 func NewDB(dbName string, pathToNewDB string) {
 	if dbName == "users" {
-		new(pathToNewDB, CreateUserTable)
+		New(pathToNewDB, CreateUserTable)
 	} else if dbName == "drives" {
-		new(pathToNewDB, CreateDriveTable)
+		New(pathToNewDB, CreateDriveTable)
 	} else if dbName == "directories" {
-		new(pathToNewDB, CreateDirectoryTable)
+		New(pathToNewDB, CreateDirectoryTable)
 	} else if dbName == "files" {
-		new(pathToNewDB, CreateFileTable)
+		New(pathToNewDB, CreateFileTable)
 	} else {
 		log.Printf("[DEBUG] unknown database category: %s", dbName)
 	}
 }
 
-// build a temp testing directory using a specified SQL query
-func NewTestDB(dbName string, pathToNewDB string, query string) {
-	db, err := sql.Open("sqlite3", pathToNewDB)
-	if err != nil {
-		log.Fatalf("[ERROR] unable to open database: \n%v\n", err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec(query)
-	if err != nil {
-		log.Fatalf("[ERROR] failed to create table: \n%v\n", err)
-	}
-}
-
-func new(path string, query string) {
+// create a new table with the given query
+func New(path string, query string) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		log.Fatalf("[ERROR] unable to open database: \n%v\n", err)
