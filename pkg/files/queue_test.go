897df41c39d99215ceddc48b70cea87bc424427a
jay <maestro1286@gmail.com>
2023-08-21 06:28:47 -0700

    add queue order test

--- pkg/files/queue_test.go
+++ pkg/files/queue_test.go
@@ -1,5 +1,38 @@
 package files
 
-import "testing"
+import (
+	"testing"
 
-func TestQueuOrder(t *testing.T) {}
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestQueueOrder(t *testing.T) {
+	testQ := NewQ()
+
+	testBatch1 := NewBatch()
+	testQ.Enqueue(testBatch1)
+
+	testBatch2 := NewBatch()
+	testQ.Enqueue(testBatch2)
+
+	testBatch3 := NewBatch()
+	testQ.Enqueue(testBatch3)
+
+	t1, err := testQ.Dequeue()
+	if err != nil {
+		Fatal(t, err)
+	}
+	assert.Equal(t, testBatch1.ID, t1.ID)
+
+	t2, err := testQ.Dequeue()
+	if err != nil {
+		Fatal(t, err)
+	}
+	assert.Equal(t, testBatch2.ID, t2.ID)
+
+	t3, err := testQ.Dequeue()
+	if err != nil {
+		Fatal(t, err)
+	}
+	assert.Equal(t, testBatch3.ID, t3.ID)
+}
