a56070718158f7dbb8ca233298cadcf2b042f425
jay <maestro1286@gmail.com>
2023-08-23 18:03:35 -0700

    add initial batch test (WIP)

--- pkg/files/batch_test.go
+++ pkg/files/batch_test.go
@@ -1,7 +1,29 @@
 package files
 
-import "testing"
+import (
+	"testing"
 
-func TestBatchAddFiles(t *testing.T) {}
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestBatchAddFiles(t *testing.T) {
+	totalFiles := RandInt(50)
+	testFiles, err := MakeABunchOfTxtFiles(totalFiles)
+	if err != nil {
+		Fatal(t, err)
+	}
+
+	// get total size of testFiles in testing dir
+	b := NewBatch()
+	b.Cap = 1000 // 1 kb batch capacity for testing
+
+	var totalSize int64
+	for _, testFile := range testFiles {
+		totalSize += testFile.Size()
+	}
+	assert.True(t, totalSize > b.Cap)
+
+	Clean(t, GetTestingDir())
+}
 
 func TestBatchLimit(t *testing.T) {}
