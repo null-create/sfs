d61c0f4976cfa0e5ec2a27ebf6608e9ec4b7908c
jay <maestro1286@gmail.com>
2023-08-23 18:02:20 -0700

    add tmp file utils

--- pkg/files/test_utils.go
+++ pkg/files/test_utils.go
@@ -8,6 +8,12 @@ import (
 	"testing"
 )
 
+// at ~1 byte per character, and at 49 characters (inlcuding spaces),
+// this string is roughly 49 bytes in size, depending on encoding.
+//
+// in go's case, this is a utf-8 encoded string, so this is roughly 49 bytes
+const txtData string = "all work and no play makes jack a dull boy"
+
 // run an individual test as part of a series of larger tests
 func RunTestStage(t *testing.T, stageName string, test func(t *testing.T)) {
 	log.Print(fmt.Sprintf("=============== %s Testing Stage ===============", stageName))
@@ -20,17 +26,53 @@ func GetTestingDir() string {
 	curDir, err := os.Getwd()
 	if err != nil {
 		log.Printf("[ERROR] unable to get testing directory: %v\ncreating...", err)
-		if err := os.Mkdir(filepath.Join(curDir, "test_files"), 0666); err != nil {
+		if err := os.Mkdir(filepath.Join(curDir, "testing"), 0666); err != nil {
 			log.Fatalf("[ERROR] unable to create test directory: %v", err)
 		}
 	}
-	return filepath.Join(curDir, "test_files")
+	return filepath.Join(curDir, "testing")
 }
 
 // handle test failures
 func Fatal(t *testing.T, err error) {
 	Clean(t, GetTestingDir())
 	t.Fatalf("[ERROR] %v", err)
+}
+
+// make a temp .txt file of n size (in bytes).
+//
+// n is determined by textReps since that will be how
+// many times testData is written to the text file
+func MakeTmpTxtFile(filePath string, textReps int) (*File, error) {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	f := NewFile(filepath.Base(filePath), "me", filePath)
+	for i := 0; i < textReps; i++ {
+		_, err := file.WriteString(txtData)
+		if err != nil {
+			return nil, fmt.Errorf("error writing to test file: %v", err)
+		}
+	}
+	return f, nil
+}
+
+// make a bunch of temp .txt files of varying sizes.
+func MakeABunchOfTxtFiles(total int) ([]*File, error) {
+	files := make([]*File, 0)
+	for i := 0; i < total; i++ {
+		fileName := fmt.Sprintf("tmp-%d.txt", i)
+		filePath := filepath.Join(GetTestingDir(), fileName)
+		f, err := MakeTmpTxtFile(filePath, RandInt(1000))
+		if err != nil {
+			return nil, fmt.Errorf("error creating temporary file: %v", err)
+		}
+		files = append(files, f)
+	}
+	return files, nil
 }
 
 // clean all contents from the testing directory
