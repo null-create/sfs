dc2345b48e70ce55f5f0c1beb6c7671fb615d632
jay <maestro1286@gmail.com>
2023-08-25 07:26:01 -0700

    remove stages

--- pkg/files/batch_test.go
+++ pkg/files/batch_test.go
@@ -7,31 +7,29 @@ import (
 )
 
 func TestBatchLimit(t *testing.T) {
-	defer RunTestStage("clean up", func() {
-		Clean(t, GetTestingDir())
-	})
+	totalFiles := RandInt(50)
+	testFiles, err := MakeABunchOfTxtFiles(totalFiles)
+	if err != nil {
+		Fatal(t, err)
+	}
 
-	RunTestStage("batch limit", func() {
-		totalFiles := RandInt(50)
-		testFiles, err := MakeABunchOfTxtFiles(totalFiles)
-		if err != nil {
-			Fatal(t, err)
-		}
+	// get total size of testFiles in testing dir
+	b := NewBatch()
+	b.Cap = 25000 // 25 kb batch capacity for testing
 
-		// get total size of testFiles in testing dir
-		b := NewBatch()
-		b.Cap = 25000 // 25 kb batch capacity for testing
+	var totalSize int64
+	for _, testFile := range testFiles {
+		totalSize += testFile.Size()
+	}
+	// make sure our test file content size is greater than our
+	// established capacity. we want to check for left over files
+	assert.True(t, totalSize > b.Cap)
 
-		var totalSize int64
-		for _, testFile := range testFiles {
-			totalSize += testFile.Size()
-		}
-		// make sure our test file content size is greater than our
-		// established capacity. we want to check for left over files
-		assert.True(t, totalSize > b.Cap)
+	// add test files
+	remTestFiles := b.AddFiles(testFiles)
+	assert.True(t, len(remTestFiles) < len(testFiles))
 
-		// add test files
-		remTestFiles := b.AddFiles(testFiles)
-		assert.True(t, len(remTestFiles) < len(testFiles))
-	})
+	if err := Clean(t, GetTestingDir()); err != nil {
+		t.Fatal(err)
+	}
 }
