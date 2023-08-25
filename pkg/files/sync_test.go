63fd83d3a8ed6a0fb29b15f31e2c8c3090b55a7f
jay <maestro1286@gmail.com>
2023-08-24 19:38:48 -0700

    add basic sync test

--- pkg/files/sync_test.go
+++ pkg/files/sync_test.go
@@ -1,5 +1,21 @@
 package files
 
-import "testing"
+import (
+	"testing"
 
-func TestMakeSyncIndex(t *testing.T) {}
+	"github.com/alecthomas/assert/v2"
+)
+
+func TestMakeSyncIndex(t *testing.T) {
+	tmpRoot := MakeTmpDirs(t)
+
+	// create a sync index from each test file
+	idx := tmpRoot.WalkS()
+	assert.NotEqual(t, nil, idx)
+
+	Clean(t, GetTestingDir())
+}
+
+// func TestBuildToUpdate(t *testing.T) {
+
+// }
