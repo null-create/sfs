56dcb12bc969406465673180fe68982d95594872
jay <maestro1286@gmail.com>
2023-08-25 21:29:55 -0700

    add RandInt

--- pkg/server/utils.go
+++ pkg/server/utils.go
@@ -4,8 +4,22 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"path/filepath"
+	"time"
 )
+
+// Generate a random integer in the range [1, n)
+func RandInt(limit int) int {
+	// Seed the random number generator with the current time
+	rand.Seed(time.Now().UnixNano())
+
+	num := rand.Intn(limit)
+	if num == 0 {
+		return 1
+	}
+	return num
+}
 
 // write out as a json file
 func saveJSON(dir, filename string, data map[string]interface{}) {
