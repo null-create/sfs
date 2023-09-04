f3e7f987594c13505f002f1e60b049f157f45d1e
jay <maestro1286@gmail.com>
2023-09-03 18:35:03 -0700

    add singleton options for query

--- pkg/db/query.go
+++ pkg/db/query.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -12,15 +13,62 @@ type Query struct {
 	Query  string
 	Debug  bool
 
-	Conn *sql.DB   // db connection
-	Stmt *sql.Stmt // SQL statement
+	Conn  *sql.DB     // db connection
+	Stmt  *sql.Stmt   // SQL statement
+	Stmts []*sql.Stmt // SQL statements (when used as a singleton)
 }
 
 // returns a new query struct
-func NewQuery(dbPath string) *Query {
+func NewQuery(dbPath string, isSingleton bool) *Query {
+	if isSingleton {
+		stmts := prepQueries(dbPath)
+		return &Query{
+			Stmts:  stmts,
+			DBPath: dbPath,
+		}
+	}
 	return &Query{
 		DBPath: dbPath,
 	}
+}
+
+func prepQueries(dbPath string) []*sql.Stmt {
+	conn, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatalf("[ERROR] unable to open database: %v", err)
+	}
+	defer conn.Close()
+
+	queries := []string{
+		CreateFileTable,
+		CreateDirectoryTable,
+		CreateDriveTable,
+		CreateUserTable,
+		DropTableQuery,
+		AddFileQuery,
+		AddDirQuery,
+		AddDriveQuery,
+		AddUserQuery,
+		FindAllUsersQuery,
+		FindAllDrivesQuery,
+		FindAllDirsQuery,
+		FindAllFilesQuery,
+		FindFileQuery,
+		FindDirQuery,
+		FindDriveQuery,
+		FindUserQuery,
+	}
+
+	stmts := make([]*sql.Stmt, 0)
+	for _, query := range queries {
+		s, err := conn.Prepare(query)
+		if err != nil {
+			log.Fatalf("failed to prepare query: %v", err)
+		}
+		stmts = append(stmts, s)
+	}
+
+	return stmts
 }
 
 // prepare a statement. must be followed by q.Stmt.Close() when called!
