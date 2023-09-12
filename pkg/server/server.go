d76aaee10a32c12f420e7131585249b70cebfc94
jay <maestro1286@gmail.com>
2023-09-11 18:54:59 -0700

    simplify server, add BuildEnv

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -15,14 +17,16 @@ type Server struct {
 
 // instantiate a new HTTP server with an sfs service instance
 func NewServer(newService bool, isAdmin bool) *Server {
-	svr := ServerConfig() // get http server and service configs
-	rtr := NewRouter()    // instantiate router
+	// load env variables from .env file
+	BuildEnv()
 
 	// initialize sfs service
 	svc, err := Init(newService, isAdmin)
 	if err != nil {
 		log.Fatalf("[ERROR] failed to initialize new service instance: %v", err)
 	}
+	svr := ServerConfig() // get http server and service configs
+	rtr := NewRouter()    // instantiate router
 
 	return &Server{
 		Svc: svc,
@@ -36,23 +40,34 @@ func NewServer(newService bool, isAdmin bool) *Server {
 	}
 }
 
-// start the server
-func (s *Server) Start() {
-	s.StartTime = time.Now()
-	if err := s.Svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("[ERROR] server startup failed: %v", err)
-	}
-}
-
-// shuts down server and returns the total run time
-func (s *Server) Shutdown() float64 {
-	if err := s.Svr.Close(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("[ERROR] server shutdown failed: %v", err)
-	}
-	return s.RunTime()
-}
-
 // returns the current run time of the server in seconds
 func (s *Server) RunTime() float64 {
 	return time.Since(s.StartTime).Seconds()
+}
+
+// start the server
+// func (s *Server) Start() {
+// 	s.StartTime = time.Now()
+// 	if err := s.Svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+// 		log.Fatalf("[ERROR] server startup failed: %v", err)
+// 	}
+// }
+
+// start the server in a separate goroutine
+func (s *Server) Start(wg *sync.WaitGroup) {
+	s.StartTime = time.Now()
+	go func() {
+		defer wg.Done() // let main know we are done cleaning up
+		if err := s.Svr.ListenAndServe(); err != nil {
+			log.Fatalf("[ERROR] failed to launch server: %v", err)
+		}
+	}()
+}
+
+// shuts down server and returns the total run time
+func (s *Server) Shutdown() error {
+	if err := s.Svr.Close(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("server shutdown failed: %v", err)
+	}
+	return nil
 }
