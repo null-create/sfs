6eb1d9767f9c051b16501e66f9c90b265689f6de
jay <maestro1286@gmail.com>
2023-09-14 06:56:15 -0700

    refactors

--- pkg/server/middleware.go
+++ pkg/server/middleware.go
@@ -17,75 +17,52 @@ func ContentTypeJson(h http.Handler) http.Handler {
 	})
 }
 
-/*
-AuthenticatedHanlder wrapper is to ensure a generic syntax
-for ensuring all handlers that need user validation get it.
+// userID := chi.URLParam(r, "userID")
+func GetAuthenticatedUser(userID string, dbDir string, w http.ResponseWriter, r *http.Request) (*auth.User, error) {
+	// TODO: validate the session token in the request
 
-It basically extends the standard http.Handler function signature by
-one argument (*auth.User).
-
-Handlers must also have the signature:
-
-	func(http.ResponseWriter, *http.Request, *auth.User)
-
-so as to be wrappable by NewEnsureAuth() middleware
-
-Examples:
-
-	mux.Handle("/v1/users/", NewEnsureAuth(UsersHandler))
-	mux.Handle("/v1/users/me", NewEnsureAuth(UsersMeHandler))
-*/
-type AuthenticatedHandler func(http.ResponseWriter, *http.Request, *auth.User)
-
-type EnsureAuth struct {
-	handler AuthenticatedHandler
-}
-
-// TODO:
-func GetAuthenticatedUser(r *http.Request) (*auth.User, error) {
-	// validate the session token in the request,
-	// fetch the session state from the session store,
-	// and return the authenticated user
-	// or an error if the user is not authenticated
-
-	return nil, nil
-}
-
-// calls GetAuthenticatedUser() prior to executing direct handler call
-func (ea *EnsureAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user, err := GetAuthenticatedUser(r)
+	// attempt to find data about the user from the the user db
+	u, err := findUser(userID, dbDir)
 	if err != nil {
-		http.Error(w, "please sign-in", http.StatusUnauthorized)
-		return
+		ServerErr(w, err.Error())
+		return nil, err
 	}
-
-	ea.handler(w, r, user)
+	return u, nil
 }
 
-func NewEnsureAuth(handlerToWrap AuthenticatedHandler) *EnsureAuth {
-	return &EnsureAuth{handlerToWrap}
-}
-
-// ------- admin router --------------------------------
-
-// A completely separate router for administrator routes
-func adminRouter() http.Handler {
-	r := chi.NewRouter()
-	r.Use(AdminOnly)
-	// TODO: admin handlers
-	// r.Get("/", adminIndex)
-	// r.Get("/accounts", adminListAccounts)
-	return r
-}
-
-func AdminOnly(h http.Handler) http.Handler {
+// get user info
+func AuthUserHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		_, ok := ctx.Value("acl.permission").(float64)
-		if !ok {
-			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		userID := chi.URLParam(r, "userID")
+		_, err := GetAuthenticatedUser(userID, "", w, r) // TODO get user db path
+		if err != nil {
+			ServerErr(w, "failed to get authenticated user")
 			return
 		}
 		h.ServeHTTP(w, r)
 	})
 }
+
+// ------- admin router --------------------------------
+
+// // A completely separate router for administrator routes
+// func adminRouter() http.Handler {
+// 	r := chi.NewRouter()
+// 	r.Use(AdminOnly)
+// 	// TODO: admin handlers
+// 	// r.Get("/", adminIndex)
+// 	// r.Get("/accounts", adminListAccounts)
+// 	return r
+// }
+
+// func AdminOnly(h http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		ctx := r.Context()
+// 		_, ok := ctx.Value("acl.permission").(float64)
+// 		if !ok {
+// 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+// 			return
+// 		}
+// 		h.ServeHTTP(w, r)
+// 	})
+// }
