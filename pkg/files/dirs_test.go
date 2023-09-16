c5cbeb6885b5a890a666142f531e557bfd249ed8
jay <maestro1286@gmail.com>
2023-09-15 19:14:32 -0700

    remove fixtures, move to utils

--- pkg/files/dirs_test.go
+++ pkg/files/dirs_test.go
@@ -3,107 +3,11 @@ package files
 import (
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
 )
-
-const TestDirName string = "testDir"
-
-//---------- test fixtures --------------------------------
-
-// creates an empty directory under ../nimbus/pkg/files/testing
-func MakeTmpDir(t *testing.T, path string) (*Directory, error) {
-	if err := os.Mkdir(path, 0666); err != nil {
-		return nil, fmt.Errorf("[ERROR] unable to create temporary directory: %v", err)
-	}
-	dir := NewDirectory("tmp", "me", path)
-	return dir, nil
-}
-
-// create a temporary root directory with files and a subdirectory,
-// also with files, under pkg/files/testing/tmp
-func MakeTmpDirs(t *testing.T) *Directory {
-	// make our temporary directory
-	d, err := MakeTmpDir(t, filepath.Join(GetTestingDir(), "tmp"))
-	if err != nil {
-		Fatal(t, err)
-	}
-
-	// create some temp files and associated file pointers
-	files, err := MakeABunchOfTxtFiles(10)
-	if err != nil {
-		Fatal(t, err)
-	}
-	tmpRoot := NewRootDirectory("root", "me", filepath.Join(GetTestingDir(), "tmp"))
-	tmpRoot.AddFiles(files)
-
-	// add a subdirectory with files so we can test traversal
-	sd, err := MakeTmpDir(t, filepath.Join(tmpRoot.Path, "tmpSubDir"))
-	if err != nil {
-		Fatal(t, err)
-	}
-
-	moreFiles := make([]*File, 0)
-	for i := 0; i < 10; i++ {
-		fname := fmt.Sprintf("tmp-%d.txt", i)
-		f, err := MakeTmpTxtFile(filepath.Join(sd.Path, fname), RandInt(100))
-		if err != nil {
-			Fatal(t, err)
-		}
-		moreFiles = append(moreFiles, f)
-	}
-
-	sd.AddFiles(moreFiles)
-	d.addSubDir(sd)
-	tmpRoot.addSubDir(d)
-
-	return tmpRoot
-}
-
-// make test files within a specified directory
-func MakeTestDirFiles(t *testing.T, total int, tdPath string) []*File {
-	testFiles := make([]*File, 0)
-
-	for i := 0; i < total; i++ {
-		name := fmt.Sprintf("test-file-%d.txt", i)
-		tfPath := filepath.Join(tdPath, name)
-
-		// Create creates or truncates the named file.
-		file, err := os.Create(tfPath)
-		if err != nil {
-			t.Fatalf("[ERROR] failed to create test file: %v", err)
-		}
-		file.Write([]byte(testData))
-		file.Close()
-
-		testFiles = append(testFiles, NewFile(name, "me", tfPath))
-	}
-
-	return testFiles
-}
-
-// makes testing/tmp directory objects.
-//
-// *does not* create actual test directories.
-// this is typically done via directory.AddSubDir()
-func MakeTestDirs(t *testing.T, total int) []*Directory {
-	testingDir := GetTestingDir()
-
-	testDirs := make([]*Directory, 0)
-	for i := 0; i < total; i++ {
-		tdName := fmt.Sprintf("%s%d", TestDirName, i)
-		tmpDirPath := filepath.Join(testingDir, tdName)
-
-		// NOTE: none of these test directories have a non-nil parent pointer
-		testDirs = append(testDirs, NewDirectory(tdName, "me", tmpDirPath))
-	}
-	return testDirs
-}
-
-// ----------------------------------------------------------------
 
 func TestSecurityFeatures(t *testing.T) {
 	testDirs := MakeTestDirs(t, 2)
