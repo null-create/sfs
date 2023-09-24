b4a96ebf4da6e2ea4f3be5d010c4e53dac38f1b8
jay <maestro1286@gmail.com>
2023-09-24 08:09:42 -0700

    add env test

--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -1,7 +1,34 @@
 package env
 
 import (
+	"os"
 	"testing"
+
+	"github.com/alecthomas/assert/v2"
 )
 
-func TestEnvBuild(t *testing.T) {}
+func buildTestEnvMap() map[string]string {
+	env := make(map[string]string)
+	env["NEW_SERVICE"] = "true"
+	env["ADMIN_MODE"] = "false"
+	env["JWT_SECRET"] = "secret-goes-here"
+	env["SERVICE_ROOT"] = "C:\\Users\\Jay\\Google Drive\\Projects\\coding\\nimbus\\pkg\\server"
+	env["SERVER_PORT"] = "8080"
+	env["SERVER_HOST"] = "localhost"
+	env["SERVER_ADDR"] = "localhost:8080"
+	env["SERVER_TIMEOUT_READ"] = "5s"
+	env["SERVER_TIMEOUT_WRITE"] = "10s"
+	env["SERVER_TIMEOUT_IDLE"] = "900s"
+	env["SERVER_ADMIN"] = "admin"
+	env["SERVER_ADMIN_KEY"] = "derp"
+	return env
+}
+
+func TestEnvBuild(t *testing.T) {
+	BuildEnv()
+	testEnv := buildTestEnvMap()
+	for k, env := range testEnv {
+		v := os.Getenv(k)
+		assert.Equal(t, v, env)
+	}
+}
