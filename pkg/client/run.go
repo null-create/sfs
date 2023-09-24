d0063fc741c01618558e375bc4086bbd6a2e5b65
jay <maestro1286@gmail.com>
2023-09-24 07:34:12 -0700

    add run files for server and client

---
+++ pkg/client/run.go
@@ -0,0 +1,9 @@
+package client
+
+/*
+top level client driver
+
+to be instantiated via the command line with a specific flag (i.e. client or server)
+*/
+
+func main() {}
---
+++ pkg/server/run.go
@@ -0,0 +1,11 @@
+package server
+
+/*
+top level server runner
+
+to be executed via command line with a specific flag (i.e. client or server)
+*/
+
+func main() {
+
+}
