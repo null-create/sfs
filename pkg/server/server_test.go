f184bceee32501d6788437df8d1e9a6c2ba2bbde
jay <maestro1286@gmail.com>
2023-09-29 15:37:42 -0700

    add simple server test

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -1,7 +1,43 @@
 package server
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"testing"
+	"time"
 
-func TestServerStartUp(t *testing.T) {}
+	"github.com/alecthomas/assert/v2"
+)
 
-func TestPingTheServer(t *testing.T) {}
+func TestServerStartUpWithAPing(t *testing.T) {
+	BuildEnv(true)
+
+	// shut down signal to the server
+	sig := make(chan bool)
+
+	// start server in its own goroutine
+	testServer := NewServer()
+	go func() {
+		testServer.TestRun(sig)
+	}()
+
+	// wait for server to start up
+	log.Printf("waiting for server to start up...")
+	time.Sleep(time.Second * 5)
+
+	// create a basic http client and ping the server at
+	// https://localhost:8080/ping
+	log.Printf("sending ping to test server...")
+	client := http.Client{Timeout: time.Second * 10}
+
+	res, err := client.Get("http://localhost:8080/ping")
+	if err != nil {
+		sig <- true // shut down test server
+		t.Fatal(err)
+	}
+	assert.Equal(t, res.StatusCode, http.StatusOK)
+
+	// TODO: figure out how to send an os signal via a channel to the server
+	// to shut it down
+	sig <- true // shut down test server
+}
