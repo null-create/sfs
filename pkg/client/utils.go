8e08cde42a6f00717ffcccad2f181a25425b1e3d
jay <maestro1286@gmail.com>
2023-10-08 20:18:35 -0700

    add utils

--- pkg/client/utils.go
+++ pkg/client/utils.go
@@ -1,9 +1,12 @@
 package client
 
 import (
+	"io"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 
 	svc "github.com/sfs/pkg/service"
 )
@@ -14,6 +17,18 @@ func GetWd() string {
 		log.Fatal(err)
 	}
 	return wd
+}
+
+// Generate a random integer in the range [1, n)
+func RandInt(limit int) int {
+	// Seed the random number generator with the current time
+	rand.Seed(time.Now().UnixNano())
+
+	num := rand.Intn(limit)
+	if num == 0 {
+		return 1
+	}
+	return num
 }
 
 // parse the NEW_SERVICE env var to see if we are instantiating a new sfs service
@@ -28,4 +43,25 @@ func isMode(k string) bool {
 		log.Fatalf("failed to parse env var string to bool: %v", err)
 	}
 	return isMode
+}
+
+// copy a file
+func Copy(src, dst string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	_, err = io.Copy(d, s)
+	if err != nil {
+		return err
+	}
+	return nil
 }
