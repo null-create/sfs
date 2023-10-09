66db1e5215168d2a70a2a86ef96d15e07850c90a
jay <maestro1286@gmail.com>
2023-10-08 19:23:38 -0700

    add util

--- pkg/client/utils.go
+++ pkg/client/utils.go
@@ -3,6 +3,9 @@ package client
 import (
 	"log"
 	"os"
+	"strconv"
+
+	svc "github.com/sfs/pkg/service"
 )
 
 func GetWd() string {
@@ -11,4 +14,18 @@ func GetWd() string {
 		log.Fatal(err)
 	}
 	return wd
+}
+
+// parse the NEW_SERVICE env var to see if we are instantiating a new sfs service
+func isMode(k string) bool {
+	env := svc.NewE()
+	v, err := env.Get(k)
+	if err != nil {
+		log.Fatalf("failed to get %s env var: %v", k, err)
+	}
+	isMode, err := strconv.ParseBool(v)
+	if err != nil {
+		log.Fatalf("failed to parse env var string to bool: %v", err)
+	}
+	return isMode
 }
