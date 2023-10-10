16b50d483087704fc44bc7314178cbf883dfc488
jay <maestro1286@gmail.com>
2023-10-09 20:27:21 -0700

    add initial client tests (wip)

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"log"
+	"os"
 	"testing"
 
 	"github.com/sfs/pkg/env"
@@ -12,26 +13,82 @@ import (
 func TestNewClient(t *testing.T) {
 	env.BuildEnv(true)
 
+	// make sure we clean the right testing directory
+	e := env.NewE()
+	tmpDir, err := e.Get("CLIENT_ROOT")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// initialize a new client
 	client, err := Init(true)
 	if err != nil {
-		if err := Clean(t, GetTestingDir()); err != nil {
-			log.Fatal(err)
-		}
+		Fail(t, tmpDir, err)
 	}
 	assert.NotEqual(t, nil, client)
 	assert.NotEqual(t, nil, client.Conf)
+	assert.NotEqual(t, "", client.User)
+	assert.NotEqual(t, "", client.UserID)
+	assert.NotEqual(t, "", client.SfDir)
+	assert.NotEqual(t, nil, client.Db)
+	assert.NotEqual(t, nil, client.client)
 
 	// check that .env was updated after initialization,
 	// specifically that CLIENT_NEW_SERVICE was set to "false"
+	v, err := e.Get("CLIENT_NEW_SERVICE")
+	if err != nil {
+		Fail(t, tmpDir, err)
+	}
+	assert.Equal(t, "false", v)
 
 	// check for service directories and necessary databases
+	entries, err := os.ReadDir(tmpDir)
+	if err != nil {
+		Fatal(t, err)
+	}
+	assert.NotEqual(t, 0, len(entries))
 
-	if err := Clean(t, client.Conf.Root); err != nil {
+	if err := Clean(t, tmpDir); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func TestLoadClient(t *testing.T) {}
+func TestLoadClient(t *testing.T) {
+	env.BuildEnv(true)
+
+	// make sure we clean the right testing directory
+	e := env.NewE()
+	tmpDir, err := e.Get("CLIENT_ROOT")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// initialize a new client, then load a new client object
+	// from the initialized service directory and state file
+	c1, err := Init(true)
+	if err != nil {
+		Fail(t, tmpDir, err)
+	}
+	if err := c1.SaveState(); err != nil {
+		Fail(t, tmpDir, err)
+	}
+
+	c2, err := Init(false)
+	if err != nil {
+		Fail(t, tmpDir, err)
+	}
+	assert.NotEqual(t, nil, c2)
+	assert.Equal(t, c1.Conf, c2.Conf)
+	assert.Equal(t, c1.User, c2.User)
+	assert.Equal(t, c1.UserID, c2.UserID)
+	assert.Equal(t, c1.SfDir, c2.SfDir)
+	assert.Equal(t, c1.Db, c2.Db)
+	assert.Equal(t, c1.client, c2.client)
+
+	if err := Clean(t, tmpDir); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func TestLoadClientSaveState(t *testing.T) {}
 
