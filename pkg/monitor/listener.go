9376675e6757c6339ed36d9ee501624cc527c90a
jay <maestro1286@gmail.com>
2023-10-22 10:49:07 -0700

    refactor

--- pkg/monitor/listener.go
+++ pkg/monitor/listener.go
@@ -7,25 +7,38 @@ This file works with the monitor to handle events and trigger
 synchronization operations accordingly.
 */
 
-type Listener struct{}
+type Listener struct {
+	Path string // path to file thats being monitored
+}
 
-func NewListener() *Listener { return &Listener{} }
+func NewListener(path string) *Listener {
+	return &Listener{
+		Path: path,
+	}
+}
 
 // listen for file events given a newly created fileChan channel.
 // ideally this function would be called soon after a new file monitor instance
 // is created, and could be used as a dedicated listener.
-func (l *Listener) Listen(stop chan bool, fileChan chan EventType) chan bool {
-	off := make(chan bool)
+//
+// listen returns an event channel that passes the detected event to the caller
+// for synchronization handling. Listen is basically a passive loop that filters
+// events for the caller, rather than handling the events directly itself.
+func (l *Listener) Listen(stop chan bool, fileChan chan EventType) (chan bool, chan EventType) {
+	off := make(chan bool)      // off switch for this listener loop
+	evt := make(chan EventType) // channel to pass Events out to sync services
 	go func() {
 		log.Print("[INFO] listening for file events...")
 		for {
 			select {
 			case event := <-fileChan:
 				switch event {
-				// TODO: handle file events lol
 				case FileCreate:
+					evt <- FileCreate
 				case FileChange:
+					evt <- FileChange
 				case FileDelete:
+					evt <- FileDelete
 				}
 			case <-stop:
 				log.Print("[INFO] stopping listener...")
@@ -35,5 +48,5 @@ func (l *Listener) Listen(stop chan bool, fileChan chan EventType) chan bool {
 	}()
 	// channel to stop monitoring
 	// (the stop parameter is this channel)
-	return off
+	return off, evt
 }
