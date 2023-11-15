6af896e9b22da465ad1abab7f0fc8ec4e613a9b0
jay <maestro1286@gmail.com>
2023-11-15 15:57:32 -0800

    create separate setup func

--- pkg/client/events.go
+++ pkg/client/events.go
@@ -81,32 +81,39 @@ func (c *Client) BuildHandlers() error {
 	return nil
 }
 
-// sets up a new listener for a given path.
-//
-// handles received events and starts transfer operations
-func EventHandler(c *Client, filePath string) error {
+// runs all necessary operations to set up an event handler
+func setupHandler(c *Client, filePath string) (chan monitor.Event, chan bool, string, *monitor.Events, error) {
 	// get event channel for this file to listen to
 	evt := c.Monitor.GetEventChan(filePath)
 	if evt == nil {
-		return fmt.Errorf("no event listener for file %s", filePath)
+		return nil, nil, "", nil, fmt.Errorf("no event listener for file %s", filePath)
 	}
 	// get off switch for this monitor
 	off := c.Monitor.GetOffSwitch(filePath)
 	if off == nil {
-		return fmt.Errorf("no shut off channel for file %s", filePath)
+		return nil, nil, "", nil, fmt.Errorf("no shut off channel for file %s", filePath)
 	}
 	// get ID for this file so we can quickly update the sync index
 	// without having to rebuild it every time
 	fileID, err := c.Db.GetFileID(filePath)
 	if err != nil {
-		return err
+		return nil, nil, "", nil, err
 	}
 	if fileID == "" {
-		return fmt.Errorf("no ID found for file %s", filePath)
+		return nil, nil, "", nil, fmt.Errorf("no ID found for file %s", filePath)
 	}
+	// new events buffer
+	evts := monitor.NewEvents(false)
+	return evt, off, fileID, evts, nil
+}
 
+// handles received events and starts transfer operations
+func EventHandler(c *Client, filePath string) error {
+	evt, off, fileID, evts, err := setupHandler(c, filePath)
+	if err != nil {
+		return err
+	}
 	go func() {
-		evts := monitor.NewEvents(false)
 		for {
 			select {
 			case e := <-evt:
