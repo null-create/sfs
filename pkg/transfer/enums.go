e87b415fd5f0a30ed46251ca551f1ec36845c6b2
jay <maestro1286@gmail.com>
2023-11-24 11:22:41 -0800

    add enums

---
+++ pkg/transfer/enums.go
@@ -0,0 +1,17 @@
+package transfer
+
+// context enums
+type Context string
+
+const (
+	Name      Context = "name"
+	File      Context = "file"
+	Email     Context = "email"
+	Admin     Context = "admin"
+	Directory Context = "directory"
+	Parent    Context = "parent"
+	Drive     Context = "drive"
+	Path      Context = "path"
+	User      Context = "user"
+	Owner     Context = "owner"
+)
