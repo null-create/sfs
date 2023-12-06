2056e4dc233e5e7cd6b17edbac058df158536fa4
jay <maestro1286@gmail.com>
2023-12-06 07:17:32 -0800

    fix comments

--- pkg/server/db.go
+++ pkg/server/db.go
@@ -18,49 +18,49 @@ func getDBConn(dbName string) *db.Query {
 	return db.NewQuery(dbPath, false)
 }
 
-// get file info from db
+// get file info from db. returns nil if not found.
 func findFile(fileID string, q *db.Query) (*svc.File, error) {
 	f, err := q.GetFile(fileID)
 	if err != nil {
 		return nil, err
 	}
-	if f == nil { // not found
+	if f == nil {
 		return nil, nil
 	}
 	return f, nil
 }
 
-// get user data from db
+// get user data from db. returns nil if not found
 func findUser(userID string, q *db.Query) (*auth.User, error) {
 	u, err := q.GetUser(userID)
 	if err != nil {
 		return nil, err
 	}
-	if u == nil { // not found
+	if u == nil {
 		return nil, nil
 	}
 	return u, nil
 }
 
-// get directory data from db
+// get directory data from db. returns nil if not found.
 func findDir(dirID string, q *db.Query) (*svc.Directory, error) {
 	d, err := q.GetDirectory(dirID)
 	if err != nil {
 		return nil, err
 	}
-	if d == nil { // not found
+	if d == nil {
 		return nil, nil
 	}
 	return d, nil
 }
 
-// get drive data from db
+// get drive data from db. returns nil if not found.
 func findDrive(driveID string, q *db.Query) (*svc.Drive, error) {
 	d, err := q.GetDrive(driveID)
 	if err != nil {
 		return nil, err
 	}
-	if d == nil { // not found
+	if d == nil {
 		return nil, nil
 	}
 	return d, nil
