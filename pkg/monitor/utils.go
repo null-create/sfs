c8f8168b25331270489abf4804ecbd5c4165c5bc
jay <maestro1286@gmail.com>
2023-12-07 20:16:02 -0800

    remove unused utils

--- pkg/monitor/utils.go
+++ pkg/monitor/utils.go
@@ -1,16 +1,11 @@
 package monitor
 
 import (
-	"encoding/json"
 	"io"
 	"log"
 	"math/rand"
 	"os"
-	"path/filepath"
-	"strconv"
 	"time"
-
-	svc "github.com/sfs/pkg/service"
 )
 
 func GetWd() string {
@@ -31,43 +26,6 @@ func RandInt(limit int) int {
 		return 1
 	}
 	return num
-}
-
-// parse the NEW_SERVICE env var to see if we are instantiating a new sfs service
-func isMode(k string) bool {
-	env := svc.NewE()
-	v, err := env.Get(k)
-	if err != nil {
-		log.Fatalf("failed to get %s env var: %v", k, err)
-	}
-	isMode, err := strconv.ParseBool(v)
-	if err != nil {
-		log.Fatalf("failed to parse env var string to bool: %v", err)
-	}
-	return isMode
-}
-
-func isEmpty(path string) bool {
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		log.Fatalf("[ERROR] %v", err)
-	}
-	if len(entries) == 0 {
-		return true
-	}
-	return false
-}
-
-// write out as a json file
-func saveJSON(dir, filename string, data map[string]interface{}) {
-	jsonData, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		log.Fatalf("[ERROR] failed marshalling JSON data: %s\n", err)
-	}
-
-	if err = os.WriteFile(filepath.Join(dir, filename), jsonData, 0666); err != nil {
-		log.Fatalf("[ERROR] unable to write JSON file %s: %s\n", filename, err)
-	}
 }
 
 // copy a file
