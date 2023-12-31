1bc5fe691bd26e4a2f8ba082cbfaa312fa3d535f
jay <maestro1286@gmail.com>
2023-12-30 20:00:30 -0800

    add file for one off calls

---
+++ pkg/client/call.go
@@ -0,0 +1,11 @@
+package client
+
+/*
+
+This file contains various implementations of specific server API calls,
+for example retrieving a single file or directory, or sending new files
+or directories to the server.const
+
+These are basically one-off functions that don't need to be part of
+the bigger push/pull synchronization events.
+*/
