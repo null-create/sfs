d6244fc5af8f1c24246d6019337d8c95c81478c8
jay <maestro1286@gmail.com>
2023-12-30 16:40:07 -0800

    add encoding

--- pkg/client/requests.go
+++ pkg/client/requests.go
@@ -1,22 +1,41 @@
 package client
 
 import (
+	"net/http"
+
 	"github.com/sfs/pkg/auth"
 	svc "github.com/sfs/pkg/service"
 )
 
 /*
-File for constructing and sending requests to the server
+File for constructing and sending requests to the server API.
 
 Will primarily use the client's built in HTTP client for this.
 Use Models package to help create requests.
 */
 
-// utils for helping create specific requests
+// utils for helping create specific http request objects
 
 // creation
 
-func (c *Client) NewUserRequest(newUser *auth.User) error { return nil }
+func (c *Client) encodeUser(user *auth.User) (string, error) {
+	tok := auth.NewT()
+	payload, err := user.ToJSON()
+	if err != nil {
+		return "", err
+	}
+	reqToken, err := tok.Create(string(payload))
+	if err != nil {
+		return "", err
+	}
+	return reqToken, nil
+}
+
+func (c *Client) NewUserRequest(newUser *auth.User) error {
+	req, err := http.NewRequest(http.MethodPost, c.Endpoints[])
+
+	return nil
+}
 
 func (c *Client) NewFileRequest(newFile *svc.File) error { return nil }
 
