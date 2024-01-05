fb43ff8752f0754ee337f3261af1262504bb508e
jay <maestro1286@gmail.com>
2024-01-05 11:55:16 -0800

    new command files

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,35 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "sfs",
+	Short: "root sfs command",
+	// long description for help messages
+	Long: `
+	A bunch of text can go here, along with some demos about how
+	to use the command.
+	
+	
+	`,
+	// filler/demo func for how to use this command
+	Run: func(cmd *cobra.Command, args []string) {
+		// whatever the command should call or do
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(rootCmd)
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- pkg/main.go
+++ pkg/main.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"github.com/sfs/cmd"
 	"github.com/sfs/pkg/env"
 )
 
@@ -16,7 +17,5 @@ import (
 
 func main() {
 	env.SetEnv(false)
-
-	// TODO: need way to shut one or both client and server down via command line
-	// server stops with ctrl-c, but not the client
+	cmd.Execute()
 }
