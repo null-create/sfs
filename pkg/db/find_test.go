d8bb87aba0a88703980fb2cf6f668d4e960dd2a6
jay <maestro1286@gmail.com>
2024-01-20 15:23:43 -0800

    new test

--- pkg/db/find_test.go
+++ pkg/db/find_test.go
@@ -28,9 +28,6 @@ func TestFindFileIdByPath(t *testing.T) {
 
 	// add file
 	if err := q.AddFile(tmpFile); err != nil {
-		if err2 := Clean(t, GetTestingDir()); err2 != nil {
-			log.Fatalf("failed to clean tmp dir: " + err2.Error())
-		}
 		Fail(t, GetTestingDir(), err)
 	}
 
@@ -52,7 +49,45 @@ func TestFindFileIdByPath(t *testing.T) {
 	}
 }
 
-func TestFindFileByName(t *testing.T) {}
+func TestFindFileByName(t *testing.T) {
+	env.SetEnv(false)
+
+	testDir := GetTestingDir()
+
+	// make testing objects
+	tmpFile, err := MakeTmpTxtFile(filepath.Join(GetTestingDir(), "tmp.txt"), RandInt(1000))
+	if err != nil {
+		Fail(t, GetTestingDir(), err)
+	}
+
+	// create tmp table
+	NewTable(filepath.Join(testDir, "tmp-db"), CreateFileTable)
+
+	// test query
+	q := NewQuery(filepath.Join(testDir, "tmp-db"), false)
+
+	// add file
+	if err := q.AddFile(tmpFile); err != nil {
+		Fail(t, GetTestingDir(), err)
+	}
+
+	// search by file name
+	file, err := q.GetFileByName(tmpFile.Name)
+	if err != nil {
+		Fail(t, GetTestingDir(), err)
+	}
+	if file == nil {
+		Fail(t, GetTestingDir(), fmt.Errorf("file not found: %v", tmpFile.Name))
+	}
+	if file.Name != tmpFile.Name {
+		Fail(t, GetTestingDir(), fmt.Errorf("file name mismatch. orig: %s new: %s", tmpFile.Name, file.Name))
+	}
+
+	// clean up tmp db
+	if err := Clean(t, GetTestingDir()); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func TestFindUserByID(t *testing.T) {}
 
