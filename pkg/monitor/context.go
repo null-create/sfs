6fc2c6052b3e3a94f9d48a95476bb4730d232e94
jay <maestro1286@gmail.com>
2024-02-17 20:36:11 -0800

    add remove + refactors

--- pkg/monitor/context.go
+++ pkg/monitor/context.go
@@ -8,11 +8,13 @@ import (
 // used for keeping track of current
 // items in the directory as its being monitored
 type DirCtx struct {
+	dirpath   string
 	currItems map[string]EItem
 }
 
-func NewDirCtx() *DirCtx {
+func NewDirCtx(dirPath string) *DirCtx {
 	return &DirCtx{
+		dirpath:   dirPath,
 		currItems: make(map[string]EItem),
 	}
 }
@@ -22,7 +24,7 @@ func (ctx *DirCtx) Clear() {
 	ctx.currItems = make(map[string]EItem)
 }
 
-func (ctx *DirCtx) HasItem(itemName string) bool {
+func (ctx *DirCtx) HaveItem(itemName string) bool {
 	if _, ok := ctx.currItems[itemName]; ok {
 		return true
 	}
@@ -31,17 +33,31 @@ func (ctx *DirCtx) HasItem(itemName string) bool {
 
 // adds all new fs.DirEntry objects to the current context and returns
 // a slice of the newly added entries.
-func (ctx *DirCtx) AddItems(new []fs.DirEntry, dirPath string) []EItem {
+func (ctx *DirCtx) AddItems(new []fs.DirEntry) []EItem {
 	diffs := make([]EItem, 0)
 	for _, item := range new {
-		if _, exist := ctx.currItems[item.Name()]; !exist {
+		if !ctx.HaveItem(item.Name()) {
 			eitem := EItem{
 				name: item.Name(),
-				path: filepath.Join(dirPath, item.Name()),
+				path: filepath.Join(ctx.dirpath, item.Name()),
 			}
 			diffs = append(diffs, eitem)
 			ctx.currItems[eitem.Name()] = eitem
 		}
+	}
+	return diffs
+}
+
+// remove items from context
+func (ctx *DirCtx) RemoveItems(remove []fs.DirEntry) []EItem {
+	diffs := make([]EItem, 0)
+	for _, item := range remove {
+		removed := EItem{
+			name: item.Name(),
+			path: filepath.Join(item.Name(), ctx.dirpath),
+		}
+		delete(ctx.currItems, item.Name())
+		diffs = append(diffs, removed)
 	}
 	return diffs
 }
