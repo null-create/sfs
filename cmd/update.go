612dcb723dcf8bd2196a6c5d33b40dcf732f3a1e
jay <maestro1286@gmail.com>
2024-02-24 20:09:49 -0800

    new cmd files

---
+++ cmd/update.go
@@ -0,0 +1,10 @@
+package cmd
+
+/*
+Command for managing various updates to various client-side only info things
+
+sfs update --user-name
+sfs update --password
+sfs update --email
+sfs update --alias
+*/
---
+++ cmd/user.go
@@ -0,0 +1,12 @@
+package cmd
+
+/*
+Command for managing the clients info
+
+NOTE: sfs update handles client info updates
+
+sfs user --new            // get a users info from the command line
+sfs user --info           // show the client info
+sfs user --remove         // remove all clients info (files, directories, etc...)
+sfs user --is-admin --key // set this user as an admin.
+*/
