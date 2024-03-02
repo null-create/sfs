980a16327512118e5c26dd73de1b10d3100cdd08
jay <maestro1286@gmail.com>
2024-03-02 10:44:08 -0800

    remove extra utils

--- pkg/server/utils.go
+++ pkg/server/utils.go
@@ -1,15 +1,10 @@
 package server
 
 import (
-	"encoding/json"
 	"log"
 	"math/rand"
 	"os"
-	"path/filepath"
-	"strconv"
 	"time"
-
-	"github.com/sfs/pkg/env"
 )
 
 // Generate a random integer in the range [1, n)
@@ -32,32 +27,6 @@ func isEmpty(path string) bool {
 		return true
 	}
 	return false
-}
-
-// parse the NEW_SERVICE env var to see if we are instantiating a new sfs service
-func isMode(k string) bool {
-	env := env.NewE()
-	v, err := env.Get(k)
-	if err != nil {
-		log.Fatalf("failed to get %s env var: %v", k, err)
-	}
-	isMode, err := strconv.ParseBool(v)
-	if err != nil {
-		log.Fatalf("failed to parse env var string to bool: %v", err)
-	}
-	return isMode
-}
-
-// write out as a json file
-func saveJSON(dir, filename string, data any) {
-	jsonData, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		log.Fatalf("[ERROR] failed marshalling JSON data: %s\n", err)
-	}
-
-	if err = os.WriteFile(filepath.Join(dir, filename), jsonData, 0666); err != nil {
-		log.Fatalf("[ERROR] unable to write JSON file %s: %s\n", filename, err)
-	}
 }
 
 func secondsToTimeStr(seconds float64) string {
