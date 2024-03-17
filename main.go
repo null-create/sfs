1b38e9d7164978297b05f6f4f4ed7fc83b8e8e1e
jay <maestro1286@gmail.com>
2024-03-17 10:33:11 -0700

    add CPU profiling (temp)

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"log"
+	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
+
 	"github.com/sfs/cmd"
 	"github.com/sfs/pkg/env"
 )
@@ -16,6 +21,16 @@ import (
 */
 
 func main() {
+	// TEMP: for cpu profiling
+	// run go tool pprof -http=localhost:6060 sfs.exe sfs.pprof to see results
+	f, err := os.Create("sfs.pprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	// main
 	env.SetEnv(false)
 	cmd.Execute()
 }
