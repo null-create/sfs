13c958d10b3883085607cb356a3c33095380d24b
jay <maestro1286@gmail.com>
2024-03-24 10:29:08 -0700

    add two new command files

---
+++ cmd/remote.go
@@ -0,0 +1,11 @@
+package cmd
+
+/*
+File for sending remote commands to the server.
+
+"Remote" meaning the CLI can directly interface with the server to
+execute commands and various upkeep.
+
+sfs remote --is-up  // see if the server is up
+sfs remote --stats  // see how long the server has been running plus some other stats?
+*/
