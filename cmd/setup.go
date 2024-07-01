2e642b6886ee991750840e6266c9abed2c3512bf
jay <maestro1286@gmail.com>
2024-07-01 16:45:50 -0700

    remove setup cmd (can be done with current CLI options)

--- cmd/setup.go
+++
@@ -1,7 +0,0 @@
-package cmd
-
-/*
-Command for setting up a new SFS filesystem and client.
-
-More explicit than sfs client --new, this will allow the user to specify their name, email, and all other service configurations
-*/
