25623fc27087c0423d69568d7c3ef5a53ed3e222
jay <maestro1286@gmail.com>
2024-07-06 12:30:13 -0700

    handle errors

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -46,7 +46,10 @@ var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-"
 func GenSecret(length int) string {
 	ll := len(chars)
 	b := make([]byte, length)
-	rand.Read(b) // generates len(b) random bytes
+	_, err := rand.Read(b) // generates len(b) random bytes
+	if err != nil {
+		log.Fatalf("failed to generate secret: %v", err)
+	}
 	for i := 0; i < length; i++ {
 		b[i] = chars[int(b[i])%ll]
 	}
