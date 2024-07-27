27019875f664b80f345a72e2962eeba54097fce5
jay <maestro1286@gmail.com>
2024-07-27 07:47:20 -0700

    add runtime output

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -119,6 +119,7 @@ func (s *Server) Start(shutDown chan bool) {
 				if _, err := s.Shutdown(); err != nil {
 					log.Fatal(err)
 				}
+				s.log.Info(fmt.Sprintf("server run time: %s", s.RunTime()))
 			}
 		}()
 
