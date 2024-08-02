2293b076790950c28107cbbeacbf7042d7b0dfad
jay <maestro1286@gmail.com>
2024-08-02 06:07:48 -0700

    no longer needed

--- pkg/server/db.go
+++ pkg/server/db.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/sfs/pkg/auth"
 	"github.com/sfs/pkg/db"
-	svc "github.com/sfs/pkg/service"
 )
 
 // ----- db utils --------------------------------
@@ -15,42 +14,6 @@ func getDBConn(dbName string) *db.Query {
 	return db.NewQuery(filepath.Join(svcCfg.SvcRoot, "dbs", dbName), false)
 }
 
-// get file info from db. file will be nil if not found.
-func findFile(fileID string, q *db.Query) (*svc.File, error) {
-	f, err := q.GetFileByID(fileID)
-	if err != nil {
-		return nil, err
-	}
-	return f, nil
-}
-
-// get a slice of all files for this user. returns an empty slice if none are found.
-func getAllFiles(userID string, q *db.Query) ([]*svc.File, error) {
-	files, err := q.GetUsersFiles(userID)
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
-}
-
-// get a slice of *all* files in the files database. returns an nil if none are available.
-func getAllTheFiles(q *db.Query) ([]*svc.File, error) {
-	files, err := q.GetFiles()
-	if err != nil {
-		return nil, err
-	}
-	return files, nil
-}
-
-// get a slice of all registered users on the server. returns an empty slice if none are found.
-func getAllUsers(adminID string, q *db.Query) ([]*auth.User, error) {
-	users, err := q.GetUsers()
-	if err != nil {
-		return nil, err
-	}
-	return users, nil
-}
-
 // get user data from db. user will be nil if not found
 func findUser(userID string, q *db.Query) (*auth.User, error) {
 	u, err := q.GetUser(userID)
@@ -58,40 +21,4 @@ func findUser(userID string, q *db.Query) (*auth.User, error) {
 		return nil, err
 	}
 	return u, nil
-}
-
-// get directory data from db. dir will be nil if not found.
-func findDir(dirID string, q *db.Query) (*svc.Directory, error) {
-	d, err := q.GetDirectoryByID(dirID)
-	if err != nil {
-		return nil, err
-	}
-	return d, nil
-}
-
-// get all users directories from the db. will return nil if none are found.
-func findAllUsersDirs(q *db.Query, userID string) ([]*svc.Directory, error) {
-	d, err := q.GetUsersDirectories(userID)
-	if err != nil {
-		return nil, err
-	}
-	return d, nil
-}
-
-// get a slice of all directories from db. dir will be nil if none are found.
-func findAllTheDirs(q *db.Query) ([]*svc.Directory, error) {
-	dirs, err := q.GetAllDirectories()
-	if err != nil {
-		return nil, err
-	}
-	return dirs, nil
-}
-
-// get drive data from db. drive will be nil if not found.
-func findDrive(driveID string, q *db.Query) (*svc.Drive, error) {
-	d, err := q.GetDrive(driveID)
-	if err != nil {
-		return nil, err
-	}
-	return d, nil
 }
