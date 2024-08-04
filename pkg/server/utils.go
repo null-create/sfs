2e2bb7771a12f4d463558d8ddc5e2171768cc1bb
jay <maestro1286@gmail.com>
2024-08-04 06:03:24 -0700

    remove commnet

--- pkg/server/utils.go
+++ pkg/server/utils.go
@@ -9,7 +9,6 @@ import (
 
 // Generate a random integer in the range [1, n)
 func RandInt(n int) int {
-	// Seed the random number generator with the current time
 	rand.Seed(time.Now().UnixNano())
 	num := rand.Intn(n)
 	if num == 0 { // don't want zero so we just autocorrect to 1 if that happens
