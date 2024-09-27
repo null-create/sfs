230980dedd5b4cdffd8cec3bc71963c5560e5a84
jay <maestro1286@gmail.com>
2024-09-27 16:31:37 -0700

    misc

--- static/scripts/frontend.js
+++ static/scripts/frontend.js
@@ -65,48 +65,6 @@ const closeMenu = (event, buttonCn, menuCn) => {
 
 // ------- uploads ---------------------------------------
 
-// add items in bulk via the /add page
-// const addItems = () => {
-//   const folderPath = document.getElementById("submit-folder-path")
-//   if (folderPath) {
-//     folderPath.addEventListener("click", () => {
-//       const spinner = document.getElementById("spinner");
-//       spinner.style.display = "block";
-      
-//       const msgElement = document.getElementById("response");
-//       msgElement.style.display = "none";
-  
-//       const pathInput = document.getElementById("folder-path-input").value;
-//       if (!pathInput) {
-//         alert("please select a path to a file or folder")
-//         return;
-//       }    
-//       console.log("path input: " + pathInput);
-
-//       fetch("/add/new", {
-//         method: "POST",
-//         body: pathInput
-//       })
-//       .then((response) => {
-//         spinner.style.display = "none";
-//         if (!response.ok) {
-//           console.error("response status: " + response.status);
-//         } else{
-//           msgElement.style.display = "block";
-//           msgElement.textContent = "Item(s) added successfully";
-//           msgElement.classList.add("success"); 
-//         }
-//       })
-//       .catch((error) => {
-//         msgElement.style.display = "block";
-//         msgElement.textContent = error.message;
-//         msgElement.classList.add("error"); 
-//         console.error("error:", error);
-//       });
-//     });
-//   }
-// }
-
 // add items by passing the path to the web client server
 document.addEventListener("DOMContentLoaded", () => {
   const folderPath = document.getElementById("submit-folder-path")
