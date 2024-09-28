6ca07e55cda07545ed18739ea11f2bb405741768
jay <maestro1286@gmail.com>
2024-09-27 17:10:26 -0700

    reintroduce profiling (still running hot)

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"log"
+	_ "net/http/pprof"
+	"runtime/pprof"
+
 	"github.com/sfs/cmd"
 	"github.com/sfs/pkg/configs"
+	"github.com/sfs/pkg/utils"
 )
 
 /*
@@ -16,6 +21,24 @@ import (
 */
 
 func main() {
+	// TEMP: for cpu and memory profiling
+	// run: go tool pprof -http=localhost:6060 ./bin/sfs.exe <cpu.pprof or mem.pprof>
+	// to see results
+	cpuFile, memFile := utils.CreatePprofFiles()
+	defer memFile.Close()
+	defer cpuFile.Close()
+
+	// cpu profiling
+	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+		log.Fatal(err)
+	}
+	defer pprof.StopCPUProfile()
+
+	// memory profiling (heap allocation)
+	if err := pprof.WriteHeapProfile(memFile); err != nil {
+		log.Fatal(err)
+	}
+
 	configs.SetEnv(false)
 	cmd.Execute()
 }
